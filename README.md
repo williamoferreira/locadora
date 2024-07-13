ded51172c1371989aeada3f99e9c6479ad271ec9
William de Oliveira Ferreira <williamoferreira@outlook.com>
2024-07-13 18:11:04 -0300

    295. Atualizando registros via PUT e PATCH

---
+++ README.md
@@ -0,0 +1,13 @@
+# APP  Locadora de carros em laravel
+## Professor: Jorge Sant'Ana
+
+## Introdução
+Implementação de sistema de gestão de locação de veículos usando Laravel 8.5.9
+
+## Inicialização
+Use o conteúdod o arquivo `.env.example` como modelo para um novo arquivo `.env`. Nesse arquivo informe o nome do Schema MySQL vazio para uso pelo sistema.
+
+Use o comando `composer i` para instalar os arquivos base do framework e em seguida use o comando `php artisan migrate` para processar a criação do banco de dados.
+
+## Testando
+use o comnando `php artisan serve` para iniciar o servidor e disponibilizar a url `localhost:8000` para visualizar o sistema.
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\LocacaoController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ModeloController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+// Route::resource('cliente', ClienteController::class);
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('carro', CarroController::class);
+Route::apiResource('locacao', LocacaoController::class);
+Route::apiResource('marca', MarcaController::class);
+Route::apiResource('modelo', ModeloController::class);
